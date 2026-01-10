d32dac2fb392cbbe35ddb157805e87e7d5ddf240
Marcel Rummens <rummens.marcel@googlemail.com>
2026-01-10 15:12:56 +0100

    feat: added run.sh

---
+++ investment-tracker/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bashio
+set -e
+
+# Export all HA add-on options as environment variables
+export FINANZBLICK_USERNAME=$(bashio::config 'FINANZBLICK_USERNAME')
+export FINANZBLICK_PASSWORD=$(bashio::config 'FINANZBLICK_PASSWORD')
+export FINANZBLICK_MFA_SECRET=$(bashio::config 'FINANZBLICK_MFA_SECRET')
+export ALPHAVANTAGE_API_KEY=$(bashio::config 'ALPHAVANTAGE_API_KEY')
+export SLACK_BOT_TOKEN=$(bashio::config 'SLACK_BOT_TOKEN')
+export MONGO_DB_CONNECTION_STRING=$(bashio::config 'MONGO_DB_CONNECTION_STRING')
+
+bashio::log.info "Starting Investment Tracker script..."
+
+# Run your original start script
+exec /opt/app-root/bin/docker_start_script.sh

18febcf4a4fc15247faf622a7e0244107f7e3846
Marcel Rummens <rummens.marcel@googlemail.com>
2026-01-10 15:30:15 +0100

    fix: bump

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -5,4 +5,6 @@ set -e
 export MONGO_INITDB_ROOT_USERNAME=$(bashio::config 'root_username')
 export MONGO_INITDB_ROOT_PASSWORD=$(bashio::config 'root_password')
 
+echo "Starting MongoDB with root user: $MONGO_INITDB_ROOT_USERNAME and password: $MONGO_INITDB_ROOT_PASSWORD"
+
 exec docker-entrypoint.sh mongod --bind_ip_all

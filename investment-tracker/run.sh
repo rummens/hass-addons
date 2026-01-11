828fe2a01017f37367254b329e949891397b5d3b
Marcel Rummens <rummens.marcel@googlemail.com>
2026-01-11 09:32:01 +0100

    fix: added debug mode

--- investment-tracker/run.sh
+++ investment-tracker/run.sh
@@ -9,8 +9,15 @@ export FINANZBLICK_MFA_SECRET=$(bashio::config 'FINANZBLICK_MFA_SECRET')
 export ALPHAVANTAGE_API_KEY=$(bashio::config 'ALPHAVANTAGE_API_KEY')
 export SLACK_BOT_TOKEN=$(bashio::config 'SLACK_BOT_TOKEN')
 export MONGO_DB_CONNECTION_STRING=$(bashio::config 'MONGO_DB_CONNECTION_STRING')
+export DEBUG=$(bashio::config 'DEBUG')
 
 bashio::log.info "Starting Investment Tracker script..."
+
+# If debug enabled run infinitive sleep loop to allow attaching debugger
+if [ "$DEBUG" = "true" ]; then
+    bashio::log.info "Debug mode enabled. Sleeping indefinitely to allow debugger attachment."
+    while true; do sleep 1000; done
+fi
 
 # Run your original app
 exec /app/docker_start_script.sh

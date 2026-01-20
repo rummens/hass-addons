66eec0bf6e08ff9077c6b2f44de5db723ab88b2c
Marcel Rummens <rummens.marcel@googlemail.com>
2026-01-20 20:46:55 +0100

    fix: removed API Key

--- investment-tracker/run.sh
+++ investment-tracker/run.sh
@@ -6,7 +6,6 @@ set -e
 export FINANZBLICK_USERNAME=$(bashio::config 'FINANZBLICK_USERNAME')
 export FINANZBLICK_PASSWORD=$(bashio::config 'FINANZBLICK_PASSWORD')
 export FINANZBLICK_MFA_SECRET=$(bashio::config 'FINANZBLICK_MFA_SECRET')
-export ALPHAVANTAGE_API_KEY=$(bashio::config 'ALPHAVANTAGE_API_KEY')
 export SLACK_BOT_TOKEN=$(bashio::config 'SLACK_BOT_TOKEN')
 export MONGO_DB_CONNECTION_STRING=$(bashio::config 'MONGO_DB_CONNECTION_STRING')
 export DEBUG=$(bashio::config 'DEBUG')
